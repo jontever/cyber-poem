16fee6673f44a0d07441a501479afa72b1407c55
jontever <75607411+jontever@users.noreply.github.com>
2023-04-30 19:14:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,31 +15,31 @@ export default function Home() {
         <p className="description">Stop and think<br>
 		Before you click that link</p>
 		
-		<p className="description">Avoid the distress from unlawful egress<br>
+		<p className="description">Avoid the distress from unlawful egress
 		of data.</p>
 		
-		<p className="description">It could be a fiend<br>
+		<p className="description">It could be a fiend
 		Who wants control of your machine</p>
 		
-		<p className="description">Phishing campaigns<br>
+		<p className="description">Phishing campaigns
 		Still remain</p>
 		
-		<p className="description">The easiest way<br>
+		<p className="description">The easiest way
 		To spread pain and dismay</p>
 		
-		<p className="description">By malicious threat actors<br>
+		<p className="description">By malicious threat actors
 		For a number of factors</p>
 		
-		<p className="description">Because the human element,<br>
+		<p className="description">Because the human element,
 		Without disrespect or detriment</p>
 		
-		<p className="description">While an organisation’s greatest asset<br>
+		<p className="description">While an organisation’s greatest asset
 		Is not immune to every facet</p>
 		
-		<p className="description">Of adversary psychology<br>
+		<p className="description">Of adversary psychology
 		And the cyber kill chain chronology</p>
 		
-		<p className="description">Weaponisation, delivery, exploitation<br>
+		<p className="description">Weaponisation, delivery, exploitation
 		Don’t click on the link, you might complete the installation!</p>
       </main>
 
