71863d6122d8d62325cce054b7b342aac12c0c78
John Doe <john.doe@contoso.com>
2023-04-30 20:01:59 +0100

    Revert "Update index.js"
    This reverts commit 008d039232230c3db5606a9a9f78ba0af63bbd5a.

--- pages/index.js
+++ pages/index.js
@@ -12,34 +12,34 @@ export default function Home() {
 
       <main>
         <Header title="Don't click on that link!" />
-        <p className="description">Stop and think<br>
+        <p>Stop and think<br>
 		Before you click that link</p>
 		
-		<p className="description">Avoid the distress from unlawful egress<br>
+		<p>Avoid the distress from unlawful egress<br>
 		of data.</p>
 		
-		<p className="description">It could be a fiend<br>
+		<p>It could be a fiend<br>
 		Who wants control of your machine</p>
 		
-		<p className="description">Phishing campaigns<br>
+		<p>Phishing campaigns<br>
 		Still remain</p>
 		
-		<p className="description">The easiest way<br>
+		<p>The easiest way<br>
 		To spread pain and dismay</p>
 		
-		<p className="description">By malicious threat actors<br>
+		<p>By malicious threat actors<br>
 		For a number of factors</p>
 		
-		<p className="description">Because the human element,<br>
+		<p>Because the human element,<br>
 		Without disrespect or detriment</p>
 		
-		<p className="description">While an organisation’s greatest asset<br>
+		<p>While an organisation’s greatest asset<br>
 		Is not immune to every facet</p>
 		
-		<p className="description">Of adversary psychology<br>
+		<p>Of adversary psychology<br>
 		And the cyber kill chain chronology</p>
 		
-		<p className="description">Weaponisation, delivery, exploitation<br>
+		<p>Weaponisation, delivery, exploitation<br>
 		Don’t click on the link, you might complete the installation!</p>
       </main>
 
