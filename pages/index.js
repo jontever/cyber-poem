f3a94f4361dc8f316ed82ac01a31a890a522601d
John Doe <john.doe@contoso.com>
2023-04-30 20:19:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,29 @@ export default function Home() {
           Stop and think, before you click that link.
         </p>
 		<p className="description">
-          Avoid the distress from unlawful egress, of data
+          Avoid the distress from unlawful egress, of data.
         </p>
+		<p className="description">
+		It could be a fiend who wants control of your machine.
+		</p>
+		<p className="description">
+		Phishing campaigns still remain, the easiest way to spread pain and dismay
+		</p>
+		<p className="description">
+		By malicious threat actors, for a number of factors.
+		</p>
+		<p className="description">
+		Because the human element, without disrespect or detriment,
+		</p>
+		<p className="description">
+		Whilst an organisation’s greatest asset, is not immune to every facet,
+		</p>
+		<p className="description">
+		Of adversary psychology and the cyber kill chain chronology.
+		</p>
+		<p className="description">
+		Weaponisation, delivery, exploitation, don’t click on the link, you might complete the installation!
+		</p>
       </main>
 
       <Footer />
