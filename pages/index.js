1c8ae9dfe6f8272d9b6a91a51aeeaa64069c6ef6
John Doe <john.doe@contoso.com>
2023-04-30 20:07:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to CSETA!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Stop and think, before you click that link.
         </p>
       </main>
 
